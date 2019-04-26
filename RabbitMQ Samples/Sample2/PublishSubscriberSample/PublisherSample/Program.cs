f81d0e86c70b4060485c113873d73746cfcb8383
Luiz Fernando Dias Rezende <lrezende@braspag.com.br>
2019-04-26 17:04:47 -0300

    including fanout sample (publish/subscriber)

---
+++ RabbitMQ Samples/Sample2/PublishSubscriberSample/PublisherSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace PublisherSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting publisher...");
+
+            using (var connection = RabbitConnectionFactory.GetConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+                    while (true)
+                    {
+                        var message = Console.ReadLine();
+
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "logs",
+                                             routingKey: "",
+                                             basicProperties: null,
+                                             body: body);
+                    }
+                }
+            }            
+        }
+    }
+}
---
+++ RabbitMQ Samples/Sample2/PublishSubscriberSample/Worker2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Worker2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Worker2...");
+
+            using (var connection = RabbitConnectionFactory.GetConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queueName, "logs", "");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
